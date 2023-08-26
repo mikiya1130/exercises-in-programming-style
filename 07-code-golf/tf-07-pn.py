c440c9a8884a465fab77888306c85b610af6784a
Morisaki Mikiya <git@morisakimikiya.com>
2023-08-26 22:40:41 +0900

    done 07

--- 07-code-golf/tf-07-pn.py
+++ 07-code-golf/tf-07-pn.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
-# My golf score is slightly lower!
-# Best wishes, Peter Norvig
 import collections
 import re
 import sys
 
-stopwords = set(open("../stop_words.txt").read().split(","))
-words = re.findall("[a-z]{2,}", open(sys.argv[1]).read().lower())
-counts = collections.Counter(w for w in words if w not in stopwords)
+stops = open("../stop_words.txt").read().split(",")
+words = re.findall(r"[a-z]{2,}", open(sys.argv[1], encoding="utf-8").read().lower())
+counts = collections.Counter(w for w in words if w not in stops)
 for w, c in counts.most_common(25):
     print(w, "-", c)
