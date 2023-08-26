eb49ff61f868b4a908b9356f222562dd4878d911
Morisaki Mikiya <git@morisakimikiya.com>
2023-08-26 23:04:36 +0900

    done 08

--- 08-infinite-mirror/tf-08.py
+++ 08-infinite-mirror/tf-08.py
@@ -3,28 +3,20 @@ import operator
 import re
 import sys
 
-# Mileage may vary. If this crashes, make it lower
 RECURSION_LIMIT = 5000
-# We add a few more, because, contrary to the name,
-# this doesn't just rule recursion: it rules the
-# depth of the call stack
 sys.setrecursionlimit(RECURSION_LIMIT + 10)
 
 
 def count(word_list, stopwords, wordfreqs):
-    # What to do with an empty list
     if word_list == []:
         return
-    # The inductive case, what to do with a list of words
     else:
-        # Process the head word
         word = word_list[0]
         if word not in stopwords:
             if word in wordfreqs:
                 wordfreqs[word] += 1
             else:
                 wordfreqs[word] = 1
-        # Process the tail
         count(word_list[1:], stopwords, wordfreqs)
 
 
@@ -38,10 +30,8 @@ def wf_print(wordfreq):
 
 
 stop_words = set(open("../stop_words.txt").read().split(","))
-words = re.findall("[a-z]{2,}", open(sys.argv[1]).read().lower())
+words = re.findall(r"[a-z]{2,}", open(sys.argv[1], encoding="utf-8").read().lower())
 word_freqs = {}
-# Theoretically, we would just call count(words, stop_words, word_freqs)
-# Try doing that and see what happens.
 for i in range(0, len(words), RECURSION_LIMIT):
     count(words[i : i + RECURSION_LIMIT], stop_words, word_freqs)
 
