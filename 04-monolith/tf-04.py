a5ff279c3dc515355e55c2dac52f977cb04c707a
Morisaki Mikiya <git@morisakimikiya.com>
2023-08-26 20:54:56 +0900

    done 04

--- 04-monolith/tf-04.py
+++ 04-monolith/tf-04.py
@@ -2,31 +2,24 @@
 import string
 import sys
 
-# the global list of [word, frequency] pairs
 word_freqs = []
-# the list of stop words
 with open("../stop_words.txt") as f:
     stop_words = f.read().split(",")
 stop_words.extend(list(string.ascii_lowercase))
 
-# iterate through the file one line at a time
-for line in open(sys.argv[1]):
+for line in open(sys.argv[1], encoding="utf-8"):
     start_char = None
     i = 0
     for c in line:
-        if start_char == None:
+        if start_char is None:
             if c.isalnum():
-                # We found the start of a word
                 start_char = i
         else:
             if not c.isalnum():
-                # We found the end of a word. Process it
                 found = False
                 word = line[start_char:i].lower()
-                # Ignore stop words
                 if word not in stop_words:
                     pair_index = 0
-                    # Let's see if it already exists
                     for pair in word_freqs:
                         if word == pair[0]:
                             pair[1] += 1
@@ -36,16 +29,13 @@ for line in open(sys.argv[1]):
                     if not found:
                         word_freqs.append([word, 1])
                     elif len(word_freqs) > 1:
-                        # We may need to reorder
                         for n in reversed(range(pair_index)):
                             if word_freqs[pair_index][1] > word_freqs[n][1]:
-                                # swap
                                 word_freqs[n], word_freqs[pair_index] = (
                                     word_freqs[pair_index],
                                     word_freqs[n],
                                 )
                                 pair_index = n
-                # Let's reset
                 start_char = None
         i += 1
 
