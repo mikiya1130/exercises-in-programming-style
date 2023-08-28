bbf119e0779ab9e5a3beae41944c7d388c5764b6
Morisaki Mikiya <git@morisakimikiya.com>
2023-08-28 14:01:00 +0900

    done 22

--- 22-tantrum/tf-22.py
+++ 22-tantrum/tf-22.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 import operator
 import re
 import string
@@ -7,25 +6,18 @@ import sys
 import traceback
 
 
-#
-# The functions
-#
 def extract_words(path_to_file):
     assert type(path_to_file) is str, "I need a string!"
     assert path_to_file, "I need a non-empty string!"
 
     try:
-        with open(path_to_file) as f:
+        with open(path_to_file, encoding="utf-8") as f:
             str_data = f.read()
     except IOError as e:
-        print(
-            "I/O error({0}) when opening {1}: {2}! I quit!".format(
-                e.errno, path_to_file, e.strerror
-            )
-        )
+        print(f"I/O error({e.errno}) when opening {path_to_file}: {e.strerror}! I quit!")
         raise e
 
-    pattern = re.compile("[\W_]+")
+    pattern = re.compile(r"[\W_]+")
     word_list = pattern.sub(" ", str_data).lower().split()
     return word_list
 
@@ -37,15 +29,11 @@ def remove_stop_words(word_list):
         with open("../stop_words.txt") as f:
             stop_words = f.read().split(",")
     except IOError as e:
-        print(
-            "I/O error({0}) when opening ../stops_words.txt: {1}! I quit!".format(
-                e.errno, e.strerror
-            )
-        )
+        print(f"I/O error({e.errno}) when opening ../stop_words.txt: {e.strerror}! I quit!")
         raise e
 
     stop_words.extend(list(string.ascii_lowercase))
-    return [w for w in word_list if not w in stop_words]
+    return [w for w in word_list if w not in stop_words]
 
 
 def frequencies(word_list):
@@ -68,13 +56,10 @@ def sort(word_freq):
     try:
         return sorted(word_freq.items(), key=operator.itemgetter(1), reverse=True)
     except Exception as e:
-        print("Sorted threw {0}".format(e))
+        print(f"Sorted threw {e}")
         raise e
 
 
-#
-# The main function
-#
 try:
     assert len(sys.argv) > 1, "You idiot! I need an input file!"
     word_freqs = sort(frequencies(remove_stop_words(extract_words(sys.argv[1]))))
@@ -84,5 +69,5 @@ try:
     for w, c in word_freqs[0:25]:
         print(w, "-", c)
 except Exception as e:
-    print("Something wrong: {0}".format(e))
+    print(f"Something wrong: {e}")
     traceback.print_exc()
