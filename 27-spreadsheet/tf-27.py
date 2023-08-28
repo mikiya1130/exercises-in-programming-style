bd17714dca626fce03fce966f95fc8e830a89173
Morisaki Mikiya <git@morisakimikiya.com>
2023-08-28 22:40:44 +0900

    done 27

--- 27-spreadsheet/tf-27.py
+++ 27-spreadsheet/tf-27.py
@@ -4,10 +4,6 @@ import operator
 import re
 import sys
 
-#
-# The columns. Each column is a data element and a formula.
-# The first 2 columns are the input data, so no formulas.
-#
 all_words = [(), None]
 stop_words = [(), None]
 non_stop_words = [
@@ -31,28 +27,19 @@ sorted_data = [
         zip(list(unique_words[0]), list(counts[0])), key=operator.itemgetter(1), reverse=True
     ),
 ]
-
-# The entire spreadsheet
 all_columns = [all_words, stop_words, non_stop_words, unique_words, counts, sorted_data]
 
 
-#
-# The active procedure over the columns of data.
-# Call this everytime the input data changes, or periodically.
-#
 def update():
     global all_columns
-    # Apply the formula in each column
     for c in all_columns:
-        if c[1] != None:
+        if c[1] is not None:
             c[0] = c[1]()
 
 
-# Load the fixed data into the first 2 columns
-all_words[0] = re.findall("[a-z]{2,}", open(sys.argv[1]).read().lower())
+all_words[0] = re.findall(r"[a-z]{2,}", open(sys.argv[1], encoding="utf-8").read().lower())
 stop_words[0] = set(open("../stop_words.txt").read().split(","))
-# Update the columns with formulas
 update()
 
-for w, c in sorted_data[0][:25]:
+for w, c in sorted_data[0][0:25]:
     print(w, "-", c)
