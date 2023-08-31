c1f3f002c8198a00145e1b748d27232f8cb0a26c
Morisaki Mikiya <git@morisakimikiya.com>
2023-08-31 17:10:57 +0900

    done 31

--- 31-map-reduce/tf-31.py
+++ 31-map-reduce/tf-31.py
@@ -6,37 +6,23 @@ import sys
 from functools import reduce
 
 
-#
-# Functions for map reduce
-#
 def partition(data_str, nlines):
-    """
-    Partitions the input data_str (a big string)
-    into chunks of nlines.
-    """
     lines = data_str.split("\n")
     for i in range(0, len(lines), nlines):
         yield "\n".join(lines[i : i + nlines])
 
 
 def split_words(data_str):
-    """
-    Takes a string,  returns a list of pairs (word, 1),
-    one for each word in the input, so
-    [(w1, 1), (w2, 1), ..., (wn, 1)]
-    """
-
     def _scan(str_data):
-        pattern = re.compile("[\W_]+")
+        pattern = re.compile(r"[\W_]+")
         return pattern.sub(" ", str_data).lower().split()
 
     def _remove_stop_words(word_list):
         with open("../stop_words.txt") as f:
             stop_words = f.read().split(",")
         stop_words.extend(list(string.ascii_lowercase))
-        return [w for w in word_list if not w in stop_words]
+        return [w for w in word_list if w not in stop_words]
 
-    # The actual work of splitting the input into words
     result = []
     words = _remove_stop_words(_scan(data_str))
     for w in words:
@@ -45,12 +31,6 @@ def split_words(data_str):
 
 
 def count_words(pairs_list_1, pairs_list_2):
-    """
-    Takes two lists of pairs of the form
-    [(w1, 1), ...]
-    and returns a list of pairs [(w1, frequency), ...],
-    where frequency is the sum of all the reported occurrences
-    """
     mapping = {}
     for pl in [pairs_list_1, pairs_list_2]:
         for p in pl:
@@ -61,11 +41,8 @@ def count_words(pairs_list_1, pairs_list_2):
     return mapping.items()
 
 
-#
-# Auxiliary functions
-#
 def read_file(path_to_file):
-    with open(path_to_file) as f:
+    with open(path_to_file, encoding="utf-8") as f:
         data = f.read()
     return data
 
@@ -74,9 +51,6 @@ def sort(word_freq):
     return sorted(word_freq, key=operator.itemgetter(1), reverse=True)
 
 
-#
-# The main function
-#
 splits = map(split_words, partition(read_file(sys.argv[1]), 200))
 word_freqs = sort(reduce(count_words, splits))
 
