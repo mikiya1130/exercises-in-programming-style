dc968144d61cdd707362159f8ac765a6646cacd7
Morisaki Mikiya <git@morisakimikiya.com>
2023-09-01 15:09:07 +0900

    done 39

--- 39-sliding-window/tf-39.py
+++ 39-sliding-window/tf-39.py
@@ -32,11 +32,9 @@ def decode_one_hot(x):
 
 
 def prepare_for_window(x):
-    # All slices of size WINDOW_SIZE, sliding through x
     ind = [np.array(np.arange(i, i + WINDOW_SIZE)) for i in range(x.shape[0] - WINDOW_SIZE + 1)]
     ind = np.array(ind, dtype=np.int32)
     x_window = x[ind]
-    # Reshape it back to a 2-d tensor
     return x_window.reshape(x_window.shape[0], x_window.shape[1] * x_window.shape[2])
 
 
@@ -44,22 +42,18 @@ def normalization_layer_set_weights(n_layer):
     wb = []
     w = np.zeros((WINDOW_SIZE * INPUT_VOCAB_SIZE, INPUT_VOCAB_SIZE))
     b = np.zeros((INPUT_VOCAB_SIZE))
-    # Let lower case letters go through
     for c in string.ascii_lowercase:
         i = char_indices[c]
         w[INPUT_VOCAB_SIZE + i, i] = 1
-    # Map capitals to lower case
     for c in string.ascii_uppercase:
         i = char_indices[c]
         il = char_indices[c.lower()]
         w[INPUT_VOCAB_SIZE + i, il] = 1
-    # Map all non-letters to space
     sp_idx = char_indices[" "]
     non_letters = [c for c in list(characters) if c not in list(string.ascii_letters)]
     for c in non_letters:
         i = char_indices[c]
         w[INPUT_VOCAB_SIZE + i, sp_idx] = 1
-    # Map single letters to space
     for c in non_letters:
         i = char_indices[c]
         w[i, sp_idx] = 0.75
@@ -72,7 +66,6 @@ def normalization_layer_set_weights(n_layer):
 
 
 def build_model():
-    # Normalize characters using a dense layer
     model = Sequential()
     model.add(
         Dense(INPUT_VOCAB_SIZE, input_shape=(WINDOW_SIZE * INPUT_VOCAB_SIZE,), activation="softmax")
@@ -84,7 +77,7 @@ model = build_model()
 model.summary()
 normalization_layer_set_weights(model.layers[0])
 
-with open(sys.argv[1]) as f:
+with open(sys.argv[1], encoding="utf-8") as f:
     for line in f:
         if line.isspace():
             continue
