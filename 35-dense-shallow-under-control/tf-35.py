46a4b33a43a9ce2e841bd9b4130408aac73be53a
Morisaki Mikiya <git@morisakimikiya.com>
2023-09-01 12:00:35 +0900

    done 35

--- 35-dense-shallow-under-control/tf-35.py
+++ 35-dense-shallow-under-control/tf-35.py
@@ -36,16 +36,13 @@ def normalization_layer_set_weights(n_layer):
     wb = []
     w = np.zeros((INPUT_VOCAB_SIZE, INPUT_VOCAB_SIZE), dtype=np.float32)
     b = np.zeros((INPUT_VOCAB_SIZE), dtype=np.float32)
-    # Let lower case letters go through
     for c in string.ascii_lowercase:
         i = char_indices[c]
         w[i, i] = 1
-    # Map capitals to lower case
     for c in string.ascii_uppercase:
         i = char_indices[c]
         il = char_indices[c.lower()]
         w[i, il] = 1
-    # Map all non-letters to space
     sp_idx = char_indices[" "]
     for c in [c for c in list(string.printable) if c not in list(string.ascii_letters)]:
         i = char_indices[c]
@@ -58,7 +55,6 @@ def normalization_layer_set_weights(n_layer):
 
 
 def build_model():
-    # Normalize characters using a dense layer
     model = Sequential()
     dense_layer = Dense(INPUT_VOCAB_SIZE, input_shape=(INPUT_VOCAB_SIZE,), activation="softmax")
     model.add(dense_layer)
@@ -69,7 +65,7 @@ model = build_model()
 model.summary()
 normalization_layer_set_weights(model.layers[0])
 
-with open(sys.argv[1]) as f:
+with open(sys.argv[1], encoding="utf-8") as f:
     for line in f:
         if line.isspace():
             continue
